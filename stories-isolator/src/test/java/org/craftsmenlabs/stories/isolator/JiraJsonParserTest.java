e6864de6bd2dadfcc013da420b227919c739e2c2
youri tjang <mail@tjang.nl>
2016-11-18 07:06:47 +0100

    unremoved old unittest

--- stories-isolator/src/test/java/org/craftsmenlabs/stories/isolator/JiraJsonParserTest.java
+++ stories-isolator/src/test/java/org/craftsmenlabs/stories/isolator/JiraJsonParserTest.java
@@ -46,4 +46,22 @@ public class JiraJsonParserTest {
                 "When I start typing and stop for 100 ms\r\n" +
                 "Then the system provides me with results that match my search string", issues.get(0).getAcceptanceCriteria());
     }
+
+    @Test
+    public void testGetJiraJsonIssuesReturnsListOnValidString() throws Exception {
+        String json = RetrieveTestData.BACKLOG_WITH_ONE_ISSUE_WITH_ACCEPTANCE_CRITERIA_FIELD;
+        List<Issue> issues = jiraJsonParser.parse(mapper.readValue(json, JiraBacklog.class));
+
+        assertEquals(issues.size(), 1);
+        assertEquals("0|zgby24:",
+                issues.get(0).getRank());
+        assertEquals("As a system administrator I want an auto-complete function that provide me with matching system results so that I can handle large datacenters\n" +
+                        "\n" +
+                        "*scope*\n" +
+                        "* A reusable UI component that can be used in all locations where we want a system administrator to manage datacenters\n" +
+                        "* The component should have an input box, and dropdown box that shows when you type at least 2 characters\n" +
+                        "* The dropdown box shows all the systems for which the description starts with the entered characters (case insensitive)",
+                issues.get(0).getUserstory());
+
+    }
 }
