58f6e657c76728b30e99593bd261501486cf8a19
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2017-01-26 10:34:41 +0100

    Revert: Stupid remote merge

---
+++ stories-storyvalidator/src/test/java/org/craftsmenlabs/stories/scoring/TeamTaskScorerTest.java
@@ -0,0 +1,68 @@
+package org.craftsmenlabs.stories.scoring;
+
+import mockit.Expectations;
+import mockit.Injectable;
+import org.apache.commons.lang3.StringUtils;
+import org.craftsmenlabs.stories.api.models.config.ValidationConfig;
+import org.craftsmenlabs.stories.api.models.items.base.TeamTask;
+import org.craftsmenlabs.stories.api.models.items.validated.ValidatedTeamTask;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TeamTaskScorerTest {
+    private TeamTaskScorer getScorer(ValidationConfig validationConfig) {
+        return new TeamTaskScorer(validationConfig);
+    }
+
+    @Test
+    public void testPerformScorerReturnsZeroOnEmpty(@Injectable ValidatedTeamTask entry, @Injectable ValidationConfig validationConfig) throws Exception {
+        new Expectations() {{
+            entry.getItem();
+            result = new TeamTask();
+
+            validationConfig.getTeamTask().getRatingThreshold();
+            result = 0.7f;
+        }};
+
+        float score = getScorer(validationConfig).validate(entry.getItem()).getPointsValuation();
+        assertThat(score).isEqualTo(0.0f);
+    }
+
+    @Test
+    @Ignore
+    public void testPerformScorerReturnsNullOnEmpty(@Injectable ValidatedTeamTask entry, @Injectable ValidationConfig validationConfig) throws Exception {
+        new Expectations() {{
+            entry.getItem();
+            result = null;
+
+            validationConfig.getTeamTask().getRatingThreshold();
+            result = 0.7f;
+        }};
+
+        float score = getScorer(validationConfig).validate(entry.getItem()).getPointsValuation();
+        assertThat(score).isEqualTo(0.0f);
+    }
+
+    @Test
+    public void testPerformScorerReturnsOneOnPerfect(@Injectable ValidatedTeamTask entry, @Injectable ValidationConfig validationConfig) throws Exception {
+        TeamTask teamTask = TeamTask.builder()
+                .summary("summary")
+                .description("description")
+                .acceptationCriteria(StringUtils.repeat("Given when then", 20))
+                .estimation(1f)
+                .build();
+        new Expectations() {{
+            entry.getItem();
+            result = teamTask;
+
+            validationConfig.getTeamTask().getRatingThreshold();
+            result = 0.7f;
+        }};
+
+        float score = getScorer(validationConfig).validate(entry.getItem()).getPointsValuation();
+        assertThat(score).isEqualTo(1.0f);
+    }
+
+}
