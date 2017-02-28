31f488f21257d1926e58d8e6b8cf1825d608873b
Mitchell Herrijgers <mitchellherrijgers@gmail.com>
2017-02-28 14:43:12 +0100

    Better default StorynatorConfig values
    (cherry picked from commit 564776c)

--- stories-api/src/main/java/org/craftsmenlabs/stories/api/models/config/StorynatorConfig.java
+++ stories-api/src/main/java/org/craftsmenlabs/stories/api/models/config/StorynatorConfig.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 @Data
 @Builder
 @NoArgsConstructor
@@ -56,12 +59,20 @@ public class StorynatorConfig {
         //quick fix because of the absence of builders/allargsconfig
         config.getValidation().getBug().setActive(true);
         config.getValidation().getBug().setRatingThreshold(0.6);
+        config.getValidation().getBug().setEnabledFields(Arrays.asList("priority", "reproduction_path", "environment", "expected_behaviour", "acceptation_criteria"));
         config.getValidation().getEpic().setActive(true);
         config.getValidation().getEpic().setRatingThreshold(0.6);
+        config.getValidation().getEpic().setEnabledFields(Collections.singletonList("goal"));
         config.getValidation().getCriteria().setActive(true);
         config.getValidation().getCriteria().setRatingThreshold(0.6);
+        config.getValidation().getCriteria().setGivenKeywords(Collections.singletonList("given"));
+        config.getValidation().getCriteria().setWhenKeywords(Collections.singletonList("when"));
+        config.getValidation().getCriteria().setThenKeywords(Collections.singletonList("then"));
         config.getValidation().getStory().setActive(true);
         config.getValidation().getStory().setRatingThreshold(0.6);
+        config.getValidation().getStory().setAsKeywords(Collections.singletonList("As a"));
+        config.getValidation().getStory().setIKeywords(Collections.singletonList("I want"));
+        config.getValidation().getStory().setSoKeywords(Collections.singletonList("So that"));
 
         return config;
     }
